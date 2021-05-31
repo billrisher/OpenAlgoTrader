f5715f49ae48fce38e4b1bbf3b192c744f2a4a0a
Bill Risher <bill@encephaloinvestments.com>
2021-05-31 14:50:12 -0500

    Beginning of pub/sub architecture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod myserver;
+mod client;
+
+use std::thread;
+use std::time::Duration;
+
+
+use myserver::run_server;
+use client::run_client;
+
 fn main() {
-    println!("Hello, World!");
+    thread::spawn(run_server);
+    run_client();
 }
